43fdf918251f4d5c173fb593adcee70ee287f1c1
JoeReiner <josephguyreiner@gmail.com>
2023-03-15 21:15:21 -0500

    3-15 - jQuery button color change

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,12 @@
 console.log("your index.js file is loaded correctly!");
 
-
+console.log("your index.js file is loaded correctly!");
+$("button").hover(function(){
+    $(this).css("background-color","#487A6C");
+  },
+  function(){
+    $(this).css("background-color", "3B6D5F");
+  });
 
 
 
