222ca4a9191a67e243ac1e7f649c6b4676527b65
amegarry <110860535+amegarry@users.noreply.github.com>
2023-03-22 15:25:15 -0500

    Revert "JQ"
    This reverts commit 3eb6c24a77ceaa43f98d6cff28c33bb2557b5e4b.

--- JS/index.js
+++ JS/index.js
@@ -8,15 +8,5 @@ $("button").hover(function(){
     $(this).css("background-color", "3B6D5F");
   });
 
-  $("button").hover(function(){
-    $(this).css("background-color","#00FF00");
-  },
-  function(){
-
-    $(this).css("background-color", "#D3D3D3.");
-
-    $(this).css("background-color","#FFFFFF");
-
-  });
 
 
